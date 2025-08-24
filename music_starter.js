f2a5d5c38f366f76b6370038fe784b3f28b6f242
lambergabr <lambertegab1210@gmail.com>
2025-08-25 09:33:10 +1200

    Colour Correction
    Assigned each bar the correct color I want for the final

--- music_starter.js
+++ music_starter.js
@@ -4,9 +4,9 @@ var colour = 0;
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
-  background(colour)
+  background(113,0,0,255)
   textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
+  rectMode(CENTER);
   textSize(24);
   
    let bar_spacing = height / 10;
@@ -16,29 +16,35 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    let background_colour1 = (214, 126, 67)
    let background_colour2 = (232, 149, 94)
 
-   colour = map(vocal,0,100, background_colour1, background_colour2); //1st ever MAP code
+   //colour = map(vocal,0,100, background_colour1, background_colour2); //1st ever MAP code
 
 // changes 
-   // vocal bar is deep desaturated red
-   fill(118,66,30);
+   // vocal bar is deep orange
+   noFill();
+   stroke(166,60,6,255);
+   strokeWeight(5);
    circle(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
    fill(0);
    text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
  
-   // drum bar is deep dark green
-   fill(70,63,44);
+   // drum bar is dark green
+   noFill();
+   stroke(149,164,114,255);
    circle(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
    fill(0);
    text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
  
-   // bass bar is desaturated green
-   fill(99,101,76);
+   // bass bar is light green
+   noFill();
+   stroke(200,224,135,255);
    circle(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
    fill(0);
    text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
  
    // other bar is eggshell white
-   fill(209,197,178);
+   noFill();
+   stroke(195,111,9,255);
+   strokeWeight(5);
    circle(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
    fill(0);
    text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
@@ -48,6 +54,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    textAlign(CENTER);
    textSize(vocal);
    text(words, width/2, height/3);
+
 
    for(let i = 0; i<5; i++) { //i++ is equivelant to i + 1
  
