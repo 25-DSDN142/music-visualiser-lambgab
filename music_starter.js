ed7f9017f3502c77e6cc4449d0f06dc00c88ae51
lambergabr <lambertegab1210@gmail.com>
2025-09-02 12:17:23 +1200

    Testing different ripple styles

--- music_starter.js
+++ music_starter.js
@@ -2,6 +2,7 @@
 var colour = 0;
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
+//Screen Resolution = 540px, 960px
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
   // 1. ~~ The background will change in relation to the vocals with Map from Deep brown to a lighter brown
@@ -23,11 +24,23 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    let ellipse_height = width / 12;
    let ellipse_pos_x = width / 2;
 
-//Randomise Ripples Position / Leave alone for now until Animation is sorted.
-   //let vocal_pos_x;
-   //let vocal_pos_y;
+//2. Randomise Ripples Position / Leave alone for now until Animation is sorted.
+
+   let vocal_pos_x;
+   let vocal_pos_y;
+
+   //let drum_pos_x;
+   //let drum_pos_y;
+
+   //let bass_pos_x;
+   //let bass_pos_y;
+
+   //let other_pos_x;
+   //let other_pos_y;
    
 //Vocal Ripple Position
+   vocal_pos_x = map(vocal, 0, 100, 0, 540);
+   vocal_pos_y = map(vocal, 0, 100, 0, 480)
 //Drum Ripple Position
 //Bass Ripple Position
 //Other Ripple Position
@@ -39,27 +52,29 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
      noFill();
      stroke(20, 93, 34);
      strokeWeight(5);
-     circle(ellipse_pos_x, height / 2 + 1 * ellipse_spacing, 4 * vocal, ellipse_height);
+     circle(vocal_pos_x, vocal_pos_y, 4 * vocal, ellipse_height);
      fill(0);
      text("vocals", ellipse_pos_x, height / 2 + 1 * ellipse_spacing + 8);
 
-     if (ellipse_height > 30){
+    //unanimated ripples js scaled up circles
+     noFill();
+     stroke(20, 93, 34);
+     strokeWeight(6);
+     circle(vocal_pos_x, vocal_pos_y, 5 * vocal, ellipse_height);
+     fill(0);
 
      noFill();
      stroke(20, 93, 34);
-     strokeWeight(5);
-     circle(ellipse_pos_x, height / 2 + 1 * ellipse_spacing, 4 * vocal, ellipse_height + 1
-     );
+     strokeWeight(7);
+     circle(vocal_pos_x, vocal_pos_y, 7 * vocal, ellipse_height);
      fill(0);
 
-     }
  
    // drum bar is dark pastel green
      noFill();
      stroke(78, 22, 55);
      circle(ellipse_pos_x, height / 2 + 2 * ellipse_spacing, 4 * drum, ellipse_height);
      fill(0);
-     text("drums", ellipse_pos_x, height / 2 + 2 * ellipse_spacing + 8);
  
    // bass bar is light yellow green
      noFill();
