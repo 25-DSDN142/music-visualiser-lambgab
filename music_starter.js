94e6e3788e6979a5f5761cfe59732fe71a628d9e
lambergabr <lambertegab1210@gmail.com>
2025-09-09 12:02:07 +1200

    Cleaning up Code and Finalising the Visuals

--- music_starter.js
+++ music_starter.js
@@ -5,42 +5,32 @@ var colour = 0;
 //Screen Resolution = 540px, 960px
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
-  // 1. DONE - The background will change in relation to the vocals with Map from Deep brown to a lighter brown
-  // 2. DONE - Change position of ripple in relation to value of vocal/drum/bass/other
-  // 3. Animating multiples ripples/ rings on every ellipse that fade out the farther they get
-
-    //1. Colour MAP
   let BG_lightness;
   BG_lightness = map(vocal, 0,100, 20, 30); 
-    //~~How do I make it change within a hue? 
-    // + change colour mode into HSL 
-    // + Callout L value and State WHAT BG_lightness and place it before the code execution 
-    // + check numbers for any typos :/
 
   background(0, 100, BG_lightness);//(113,0,0,255); //deep brown red
   colorMode(HSL)
 
-//2. Randomise Ripples Position +  Animating multiples ripples/ rings on every ellipse that fade out the farther they get
-  //RIPPLES
+
 
    //VOCAL bar is deep orange + reactive placement
    if(vocal<50){
      noFill();
      stroke(20, 93, 34);
      strokeWeight(5);
-     circle(100, 200, 4 * vocal); 
+     circle(100, 125, 4 * vocal); 
 
       //Drawn on Ripples are just scaled up circles
       noFill();
       stroke(13, 88, 34);
       strokeWeight(6);
-      circle(100, 200, 5 * vocal);
+      circle(100, 125, 5 * vocal);
       noFill();
 
       noFill();
       stroke(13, 88, 31);
       strokeWeight(7);
-      circle(100, 200, 7 * vocal);
+      circle(100, 125, 8 * vocal);
       noFill();
    }
 
@@ -70,14 +60,14 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
      noFill();
      stroke(78, 22, 55);
      strokeWeight(3);
-     circle(350, 300, 5*drum);
+     circle(350, 200, 5*drum);
      noFill();
 
     //Drawn on Ripples are just scaled up circles
     noFill();
     stroke(65, 20, 42);
     strokeWeight(5);
-    circle(350, 300, 6*drum);
+    circle(350, 200, 6*drum);
     noFill();
    }
 
@@ -100,7 +90,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
  
    // BASS bar is light yellow green + using IF statements for reactive placement 
 
-   if (bass>45) { 
+   if (bass>50) { 
     noFill();
     stroke(76, 59, 70);
     circle(70, 750, 4 * bass);
@@ -120,7 +110,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
       noFill();
    }
 
-   if (bass<40) { //~~bass is less than 75 but greater than 50 execute:
+   if (bass<50) {
     noFill();
     stroke(57, 44, 62);
     strokeWeight(3);
@@ -135,28 +125,15 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    }
   
  
-   // OTHER bar is bright orange = using IF statements for reactive placement
-let other_posX = 350;
-let other_posY = 500;
+   // OTHER bar is bright orange + using IF statements for reactive placement
 
-other_posY = other_posY +5
-other_posX = other_posX +5
+    noFill();
+    stroke(33, 91, 40);
+    strokeWeight(2);
+    circle(0, 960, 15 * other);
 
-   if(other>80) {
-     noFill();
-     stroke(33, 91, 40);
-     strokeWeight(2);
-     line(0, other_posY, other_posX, 960);
-    
-     noFill();
-     stroke(30, 86, 35);
-     strokeWeight(4);
-     line(0, 480, 370, 960);
-
-     noFill();
-     stroke(18, 83, 31);
-     strokeWeight(7);
-     line(0, 450, 400, 960);
-   }
-
+    noFill();
+    stroke(33, 91, 40);
+    strokeWeight(5);
+    circle(0, 960, 20 * other);
 }
